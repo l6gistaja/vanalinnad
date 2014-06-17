63661e77f2a24b9b0f0ec338e5d40441c4bd675b
Juks <juks.nospam@alkohol.ee>
2014-06-18 00:28:24 +0300

    Tallinn roads minified

--- dev/kml_minify.pl
+++ dev/kml_minify.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
 
-if(scalar(@ARGV) < 2) {
-  print "\nUsage: kml_minify.pl PRECISION SOURCE_KML_FILE_PATH";
+if(scalar(@ARGV) < 3) {
+  print "\nUsage: kml_minify.pl PRECISION SOURCE_KML_FILE_PATH RESULT_KML_FILE_PATH";
   print "\nPRECISION examples: .00001 = 1.1 m of latitude, .000001 = 0.11 m of latitude\n\n";
   exit;
 }
@@ -17,25 +17,28 @@ $xml = new XML::Simple;
 $kml = $xml->XMLin($ARGV[1], ForceArray => 1);
 #use Data::Dumper; print Dumper($kml);
 
-print <<EndHeader;
+open (KML, '>'.$ARGV[2]) or die("Could not open file ".$ARGV[2]);
+binmode KML, ":utf8";
+
+print KML <<EndHeader;
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://earth.google.com/kml/2.2">
 <Document>
 EndHeader
 
 foreach $placemark (@{$kml->{'Document'}[0]->{'Placemark'}}) {
-  print '<Placemark>'.$br;
+  print KML '<Placemark>'.$br;
   foreach $k (keys %{$placemark}) {
     if($k eq 'MultiGeometry') {
-      print '<MultiGeometry>'.$br;
+      print KML '<MultiGeometry>'.$br;
       foreach $linestr (@{$placemark->{'MultiGeometry'}[0]->{'LineString'}}) {
-        print '<LineString><coordinates>'.
+        print KML '<LineString><coordinates>'.
           format_coords($linestr->{'coordinates'}[0], $precision).
           '</coordinates></LineString>'.$br;
       }
-      print '</MultiGeometry>'.$br;
+      print KML '</MultiGeometry>'.$br;
     } elsif($k eq 'LineString') {
-      print '<LineString><coordinates>'.
+      print KML '<LineString><coordinates>'.
           format_coords($placemark->{'LineString'}[0]->{'coordinates'}[0], $precision).
           '</coordinates></LineString>'.$br;
     } else {
@@ -43,17 +46,20 @@ foreach $placemark (@{$kml->{'Document'}[0]->{'Placemark'}}) {
       $v =~ s/&/&amp;/g;
       $v =~ s/</&lt;/g;
       $v =~ s/>/&gt;/g;
-      print '<'.$k.'>'.$v.'</'.$k.'>'.$br;
+      print KML '<'.$k.'>'.$v.'</'.$k.'>'.$br;
     }
   }
-  print '</Placemark>'.$br;
+  print KML '</Placemark>'.$br;
 }
 
-print <<EndHeader;
+print KML <<EndHeader;
 
 </Document>
 </kml>
 EndHeader
+
+
+close(KML) or die("Could not close file ".$ARGV[1]);
 
 sub format_coords {
   $y = $_[0];
