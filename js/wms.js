3a11438e3a2df49da83bd7a0f7ac5100205feea4
jux <jux@pill>
2016-01-22 00:49:31 +0200

    WMS draw functionality + Tyri-Paide railway

--- js/wms.js
+++ js/wms.js
@@ -116,33 +116,50 @@ function vlWms(inputParams){
       };
       if(!('draw' in reqParams)) { vlUtils.mapAddCoordsPromptCtl(map, clickData); }
       
-      if('draw' in reqParams) {
-        var lineLayer = new OpenLayers.Layer.Vector("Line Layer",{styleMap: vlUtils.mergeCustomStyleWithDefaults(jsonConf.olLayerStyles['roads'])});
-        map.addLayer(lineLayer);
-	var drawCtl = new OpenLayers.Control.DrawFeature(lineLayer, OpenLayers.Handler.Path);
-	map.addControl(drawCtl);
-	drawCtl.activate();
-      function showPath() { 
-	var y = '';
-	for(var i = 0; i < lineLayer.features.length; i++) {
-		y += "<LineString><coordinates>";
-		for(var j = 0; j < lineLayer.features[i].geometry.components.length; j++) {
-			var x = lineLayer.features[i].geometry.components[j].x;
-			var y = lineLayer.features[i].geometry.components[j].y;
-			y += x + "," + y + " ";
-		}
-		y += "</coordinates></LineString>\n";
-	}
-      }
-      var drawBtn = new OpenLayers.Control.Button({
-        displayClass: 'drawBtn',
-        title: "Draw line",
-        trigger: showPath
-      });
-      var drawPanel = new OpenLayers.Control.Panel({defaultControl: drawBtn});
-      drawPanel.addControls([drawBtn]);
-	map.addControl(drawPanel);
-      }
+        if('draw' in reqParams) {
+
+            var lineLayer = new OpenLayers.Layer.Vector("DRAW",
+                {styleMap: vlUtils.mergeCustomStyleWithDefaults(jsonConf.olLayerStyles['roads'])});
+            map.addLayer(lineLayer);
+            var drawCtl = new OpenLayers.Control.DrawFeature(lineLayer, OpenLayers.Handler.Path);
+            map.addControl(drawCtl);
+            drawCtl.activate();
+
+            function showPath() { 
+                var path = '';
+                var coords;
+                for(var i = 0; i < lineLayer.features.length; i++) {
+                    path += "<LineString><coordinates>";
+                    for(var j = 0; j < lineLayer.features[i].geometry.components.length; j++) {
+                        coords = [lineLayer.features[i].geometry.components[j].x,
+                            lineLayer.features[i].geometry.components[j].y];
+                        if(jsonConfWMS.mapoptions.projection != 'EPSG:4326') {
+                            try {
+                                var coords = proj4(
+                                    jsonConf.proj4[jsonConfWMS.mapoptions.projection.replace(':','_')],
+                                    jsonConf.proj4['EPSG_4326'],
+                                    coords);
+                            } catch(err) {
+                                alert('PROJ4 failure.');
+                                return;
+                            }
+                        }
+                        path += coords[0] + "," + coords[1] + " ";
+                    }
+                    path = path.trim() + "</coordinates></LineString>";
+                }
+                prompt('Lines as KML LineStrings', path);
+            }
+            var drawBtn = new OpenLayers.Control.Button({
+                displayClass: 'drawBtn',
+                title: "Draw line",
+                trigger: showPath
+            });
+            var drawPanel = new OpenLayers.Control.Panel({defaultControl: drawBtn});
+            drawPanel.addControls([drawBtn]);
+            map.addControl(drawPanel);
+
+        }
 
     } else {
       window.location.replace('?error=nonexisting_wms');
