35dd6f10f88a8926b59f465ffa9bbdb7f5f00cc9
Juks <juks@alkohol.ee>
2019-09-23 22:55:46 +0300

    1

--- 1.php
+++ 1.php
@@ -1,3 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 echo 1;
