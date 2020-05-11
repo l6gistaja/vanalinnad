5857243949cff2d2e169e2d04446ad83c80383c7
Juks <juks@alkohol.ee>
2020-05-11 21:16:20 +0300

    Paldiski 1951

--- loads/downloads.bash
+++ loads/downloads.bash
@@ -983,10 +983,8 @@ dev/deiip.pl -u 'http://www.ra.ee/fcgi-bin/iipsrv.fcgi?FIF=/mnt/saaga_laiendus/k
 # Haapsalu 1907 https://www.digar.ee/arhiiv/nlib-digar:397431 -
 # Kuressaare 1920 http://www.ra.ee/kaardid/index.php/et/map/view?id=206752
 # Narva 1918 http://www.ra.ee/kaardid/index.php/et/map/view?id=131763
-# Paide 1907 http://www.ra.ee/kaardid/index.php/et/map/view?id=101426
+# Paide 1907 http://www.ra.ee/kaardid/index.php/et/map/view?id=101426 -
 # Paldiski 1918 http://www.ra.ee/kaardid/index.php/et/map/view?id=102202 http://www.ra.ee/kaardid/index.php/et/map/view?id=200641 -
 # Paldiski 1915 https://www.digar.ee/viewer/et/nlib-digar:356994/311168/page/1 -
-# Parnu 1911 http://www.ra.ee/kaardid/index.php/et/map/view?id=101352 https://www.digar.ee/arhiiv/nlib-digar:357521
-# Parnu 1960 http://www.ra.ee/kaardid/index.php/et/map/view?id=29951
 # Rakvere 1915 https://www.digar.ee/viewer/et/nlib-digar:356961/311205/page/1 http://www.ra.ee/kaardid/index.php/et/map/view?id=102760 -
 # Viljandi ~1800 http://www.ra.ee/kaardid/index.php/et/map/view?id=15670 -
