140c6236189e72b41def9fc0102cc17b29256cc1
Juks <juks@alkohol.ee>
2021-12-25 14:43:32 +0200

    Narva roads

--- dev/tiler.pl
+++ dev/tiler.pl
@@ -38,13 +38,13 @@ if($c{'y'} < 0) {
 if(exists $gdal->{'translate'}[$c{'y'}]{'composite'}) {
   $c{'composite'} = $gdal->{'translate'}[$c{'y'}]{'composite'};
   $c{'dirsrcimg'} = $opts{'s'}.'/'.$mainconf->{'dircomposite'}.$c{'composite'}.'/';
-  $c{'dirraster'} = $c{'dirsrcimg'}.$opts{'y'};
 } else {
   $c{'dirsrcimg'} = $opts{'s'}.'/';
-  $c{'dirraster'} = $mainconf->{'dirraster'}.$mainconf->{'dirplaces'}.$opts{'s'}.'/'.$opts{'y'};
 }
 $c{'diroriginals'} = $localdata{'diroriginals'}.$c{'dirsrcimg'};
 $c{'dirsrcimg'} = $localdata{'dirsource'}.$c{'dirsrcimg'};
+$c{'dirraster'} = exists $gdal->{'translate'}[$c{'y'}]{'composite'} 
+    ? $c{'dirsrcimg'}.$opts{'y'} : $mainconf->{'dirraster'}.$mainconf->{'dirplaces'}.$opts{'s'}.'/'.$opts{'y'};
 $c{'tlast'} = gdal_tlast($gdal, $c{'y'});
 $c{'filesrcimg'} = $c{'diroriginals'}.$gdal->{'translate'}[$c{'y'}]{'t'}[$c{'tlast'}]{'file'};
 $c{'dirtransform'} = $mainconf->{'dircache'}.'transform/';
